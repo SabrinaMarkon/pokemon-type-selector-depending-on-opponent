cdd94f7abeb9297a90ad7576701b433ce26f01d4
Sabrina Markon <phpsitescripts@outlook.com>
2018-01-02 21:18:40 -0700

    User select box of Pokemon types outputs type to the console.

--- pokemontypes.js
+++ pokemontypes.js
@@ -1,35 +1,68 @@
-// Pick a pokemon type and see which other types are most powerful against it.
+/**
+ * @author Sabrina Markon
+ * @version 1.0
+ * January 2, 2018 Happy New Year! :)
+ * Pick a pokemon type and see which other types are most powerful against it.
+ */
 // all 18 pokemon types (as of January 2018)
-var PokemonType;
-(function (PokemonType) {
-    PokemonType["Bug"] = "Fire, Flying, Rock";
-    PokemonType["Electric"] = "Ground";
-    PokemonType["Fire"] = "Water, Ground, Rock";
-    PokemonType["Grass"] = "Fire, Ice, Poison, Flying, Bug";
-    PokemonType["Normal"] = "Fighting";
-    PokemonType["Rock"] = "Water, Grass, Fighting, Ground, Steel";
-    PokemonType["Dark"] = "Fighting, Bug, Fairy";
-    PokemonType["Fairy"] = "Poison, Steel";
-    PokemonType["Flying"] = "Electric, Ice, Rock";
-    PokemonType["Ground"] = "Water, Grass, Ice";
-    PokemonType["Poison"] = "Ground, Psychic";
-    PokemonType["Steel"] = "Fire, Fight, Ground";
-    PokemonType["Dragon"] = "Ice, Dragon, Fairy";
-    PokemonType["Fighting"] = "Fying, Psychic, Fairy";
-    PokemonType["Ghost"] = "Ghost, Dark";
-    PokemonType["Ice"] = "Fire, Fight, Rock, Steel";
-    PokemonType["Psychic"] = "Bug, Ghost, Dark";
-    PokemonType["Water"] = "Electric, Grass";
-})(PokemonType || (PokemonType = {}));
-var p = PokemonType.Poison;
-alert(p);
-var PokemonWeaknesses = /** @class */ (function () {
-    function PokemonWeaknesses(pokemontype) {
+var PokemonTypes;
+(function (PokemonTypes) {
+    PokemonTypes["Bug"] = "Fire, Flying, Rock";
+    PokemonTypes["Electric"] = "Ground";
+    PokemonTypes["Fire"] = "Water, Ground, Rock";
+    PokemonTypes["Grass"] = "Fire, Ice, Poison, Flying, Bug";
+    PokemonTypes["Normal"] = "Fighting";
+    PokemonTypes["Rock"] = "Water, Grass, Fighting, Ground, Steel";
+    PokemonTypes["Dark"] = "Fighting, Bug, Fairy";
+    PokemonTypes["Fairy"] = "Poison, Steel";
+    PokemonTypes["Flying"] = "Electric, Ice, Rock";
+    PokemonTypes["Ground"] = "Water, Grass, Ice";
+    PokemonTypes["Poison"] = "Ground, Psychic";
+    PokemonTypes["Steel"] = "Fire, Fight, Ground";
+    PokemonTypes["Dragon"] = "Ice, Dragon, Fairy";
+    PokemonTypes["Fighting"] = "Fying, Psychic, Fairy";
+    PokemonTypes["Ghost"] = "Ghost, Dark";
+    PokemonTypes["Ice"] = "Fire, Fight, Rock, Steel";
+    PokemonTypes["Psychic"] = "Bug, Ghost, Dark";
+    PokemonTypes["Water"] = "Electric, Grass";
+})(PokemonTypes || (PokemonTypes = {}));
+// get all pokemon type names to populate the dropdown box and store in array.
+//let allpokemontypes: Array<string> = [];
+// make select box:
+var selectbox = document.createElement('select');
+selectbox.id = 'pokemontype';
+// empty default option:
+var option = document.createElement('option');
+option.value = 'none';
+option.text = "Select a Pokemon Type!";
+selectbox.appendChild(option);
+for (var enumPokemonName in PokemonTypes) {
+    //allpokemontypes.push(enumPokemonName);
+    var option_1 = document.createElement('option');
+    option_1.value = enumPokemonName;
+    option_1.text = enumPokemonName;
+    selectbox.appendChild(option_1);
+}
+//console.log(selectbox);
+document.body.appendChild(selectbox);
+selectbox.onchange = function (event) {
+    var selectvalue = document.getElementById(selectbox.id).value;
+    if (selectvalue === 'none') {
+        // console.log('none');
+        // nothing should happen
+    }
+    else {
+        console.log(selectvalue);
+    }
+};
+var Weaknesses = /** @class */ (function () {
+    function Weaknesses(pokemontype) {
         this.pokemontype = pokemontype;
     }
-    PokemonWeaknesses.prototype.getWeaknesses = function (pokemontype) {
+    Weaknesses.prototype.getWeaknesses = function (pokemontype) {
         var weakagainst; // get the right weakness when given the type.
         return weakagainst;
     };
-    return PokemonWeaknesses;
+    Weaknesses.alltypes = PokemonTypes;
+    return Weaknesses;
 }());
--- pokemontypes.ts
+++ pokemontypes.ts
@@ -1,4 +1,9 @@
-// Pick a pokemon type and see which other types are most powerful against it.
+/**
+ * @author Sabrina Markon
+ * @version 1.0
+ * January 2, 2018 Happy New Year! :)
+ * Pick a pokemon type and see which other types are most powerful against it.
+ */ 
 
 // all 18 pokemon types (as of January 2018)
 enum PokemonTypes {
@@ -21,6 +26,39 @@ enum PokemonTypes {
     Psychic = 'Bug, Ghost, Dark',
     Water = 'Electric, Grass'
 }
+
+// get all pokemon type names to populate the dropdown box and store in array.
+//let allpokemontypes: Array<string> = [];
+
+// make select box:
+let selectbox: HTMLElement = document.createElement('select');
+selectbox.id = 'pokemontype';
+// empty default option:
+let option = document.createElement('option');
+option.value = 'none';
+option.text = "Select a Pokemon Type!";
+selectbox.appendChild(option);
+
+for (var enumPokemonName in PokemonTypes) {
+    //allpokemontypes.push(enumPokemonName);
+    let option = document.createElement('option');
+    option.value = enumPokemonName;
+    option.text = enumPokemonName;
+    selectbox.appendChild(option);    
+ }
+
+ //console.log(selectbox);
+document.body.appendChild(selectbox);
+
+ selectbox.onchange = (event) => {
+    let selectvalue: string = (<HTMLInputElement>document.getElementById(selectbox.id)).value;
+    if (selectvalue === 'none') {
+        // console.log('none');
+        // nothing should happen
+    } else {
+        console.log(selectvalue);
+    }
+ }
 
 // interface for describing elements we need.
 // select box is to pick the pokemon type.
